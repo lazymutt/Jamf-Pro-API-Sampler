a9b1e2a99573ee21b785ec163483679ea1d26c40
Todd McDaniel <lazymutt@mac.com>
2022-03-03 13:49:54 -0800

    Added invalidation, new urls
    Added invalidating issued token
    Switched to non-preview URLs
    Spiffed up the code a bit

--- uapi_demo.py
+++ uapi_demo.py
@@ -1,21 +1,35 @@
 #!/usr/bin/env python3
 
 import requests
-import json
 from local_credentials import jamf_user, jamf_password, jamf_hostname
 
-def get_uapi_token(jamf_user, jamf_password, jamf_hostname):
 
-    jamf_test_url = jamf_hostname + "/uapi/auth/tokens"
+def get_uapi_token():
+
+    jamf_test_url = jamf_hostname + "/api/v1/auth/token"
     headers = {'Accept': 'application/json', }
     response = requests.post(url=jamf_test_url, headers=headers, auth=(jamf_user, jamf_password))
     response_json = response.json()
+
     return response_json['token']
-	
+
+
+def invalidate_uapi_token(uapi_token):
+
+    jamf_test_url = jamf_hostname + "/api/v1/auth/invalidate-token"
+    headers = {'Accept': '*/*', 'Authorization': 'Bearer ' + uapi_token}
+    response = requests.post(url=jamf_test_url, headers=headers)
+
+    if response.status_code == 204:
+        print('Token invalidated!')
+    else:
+        print('Error invalidating token.')
+
+
 def main():
 
     # fetch Jamf Pro (ex-universal) api token
-    uapi_token = get_uapi_token(jamf_user, jamf_password, jamf_hostname)    
+    uapi_token = get_uapi_token()
 
     # fetch sample Jamf Pro api call
     headers = {'Accept': 'application/json', 'Authorization': 'Bearer ' + uapi_token}
@@ -23,6 +37,11 @@ def main():
     response_json = response.json()
 
     print(response_json['version'])
+
+    # invalidating token
+    print('invalidating token...')
+    invalidate_uapi_token(uapi_token)
+
 
 if __name__ == '__main__':
     main()
