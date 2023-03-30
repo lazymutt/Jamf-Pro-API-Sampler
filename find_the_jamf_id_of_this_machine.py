92e11a222b4ca6d8db25922d072d4c279d762a93
Todd McDaniel <lazymutt@mac.com>
2023-03-30 14:12:25 -0700

    simplified
    switched to json output from system_profiler, removed grep

--- find_the_jamf_id_of_this_machine.py
+++ find_the_jamf_id_of_this_machine.py
@@ -1,25 +1,23 @@
-#!/usr/local/bin/python3
-
-import re
+#!/usr/bin/env python3
+import json
 import subprocess
 import requests
 from local_credentials import jamf_user, jamf_password, jamf_hostname
 
 
 def main():
-    local_uuid_raw = subprocess.check_output(["system_profiler", "SPHardwareDataType"]).decode('utf-8')
-    local_uuid = re.findall(r'Hardware UUID: (.*)', local_uuid_raw)[0]
-
-    print(local_uuid)
+    local_hardware_raw = subprocess.check_output(['/usr/sbin/system_profiler', 'SPHardwareDataType', '-json']).decode('utf-8')
+    hardware = json.loads(local_hardware_raw)
+    local_uuid = hardware['SPHardwareDataType'][0]['platform_UUID']
 
     jamf_test_url = jamf_hostname + '/JSSResource/computers/udid/' + local_uuid
     headers = {'Accept': 'application/json', }
-    response = requests.get(url=jamf_test_url, headers=headers, auth=(jamf_user, jamf_password))
+    response = requests.get(url=jamf_test_url, headers=headers, auth=(jamf_user, jamf_password), timeout=3)
 
     if response.status_code == 200:
         response_json = response.json()
         local_jamf_id = response_json['computer']['general']['id']
-        print(local_jamf_id)
+        print(f"JAMF ID: {local_jamf_id}")
     else:
         print("Error.")
 
